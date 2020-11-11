3d589d34ff6c6c390233b1528c540a1bf25027e1
TomKellyGenetics <tomkellygenetics@gmail.com>
2020-11-11 13:59:33 +0900

    correct handing of branches for GitHub Actions

--- wrapper.sh
+++ wrapper.sh
@@ -557,6 +557,15 @@ if [[ $render == true ]]; then
 #      fi
 #    done  
 
+    if [[ ! -d ../${repo} ]]; then
+        if [[ -z $GITHUB_TOKEN ]]; then
+            url=https://github.com/${remote_user}/${repo}.git
+        else
+            url="https://${git_user}:${GITHUB_TOKEN}@github.com/${remote_user}/${repo}.git"
+        fi
+    fi
+
+
     #create external repo
     mkdir -p ../${repo}
 
